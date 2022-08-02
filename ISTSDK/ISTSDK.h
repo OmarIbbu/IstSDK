4ab5985678ee47398f4165dec5f0eadbf766d7b5
OmarIbbu <omar17farooq@gmail.coom>
2022-08-02 11:59:22 +0530

    Initial Commit

---
+++ ISTSDK/ISTSDK.h
@@ -0,0 +1,18 @@
+//
+//  ISTSDK.h
+//  ISTSDK
+//
+//  Created by Umar Farooq on 02/08/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ISTSDK.
+FOUNDATION_EXPORT double ISTSDKVersionNumber;
+
+//! Project version string for ISTSDK.
+FOUNDATION_EXPORT const unsigned char ISTSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ISTSDK/PublicHeader.h>
+
+
